17b609a47ce02005e661f4dbbb5b0448f20ea3f2
Victor Sokolov <gzigzigzeo@evilmartians.com>
2016-04-01 20:37:59 +0400

    Finding failing spec

--- spec/ebay_request/finding_spec.rb
+++ spec/ebay_request/finding_spec.rb
@@ -14,48 +14,40 @@ describe EbayRequest::Finding do
     allow(EbayRequest).to receive(:config).and_return(config)
   end
 
-  context "#response" do
-    let(:failing_request) do
-      "{\"jsonns.xsi\":\"http://www.w3.org/2001/XMLSchema-instance\",\
+  let(:failing_request) do
+    "{\"jsonns.xsi\":\"http://www.w3.org/2001/XMLSchema-instance\",\
 \"jsonns.xs\":\"http://www.w3.org/2001/XMLSchema\",\"jsonns.tns\"\
 :\"http://www.ebay.com/marketplace/search/v1/services\",\
 \"tns.findItemsByKeywordsRequest\":{}}"
-    end
+  end
 
-    let(:failing_response) do
-      "{\"findItemsByKeywordsResponse\":[{\"ack\":[\"Failure\"],\
+  let(:failing_response) do
+    "{\"findItemsByKeywordsResponse\":[{\"ack\":[\"Failure\"],\
 \"errorMessage\":[{\"error\":[{\"errorId\":[\"2\"],\"domain\":\
 [\"Marketplace\"],\"severity\":[\"Error\"],\"category\":[\"Request\"],\
 \"message\":[\"Keywords value required.\"],\"subdomain\":[\"Search\"]}]}]\
 ,\"version\":[\"1.13.0\"],\"timestamp\":[\"2016-04-01T16:22:50.643Z\"]}]}"
-    end
+  end
 
-    it "with errors" do
-      stub_request(
-        :post, "http://svcs.sandbox.ebay.com/services/search/FindingService/v1"
+  it "#response" do
+    stub_request(
+      :post, "http://svcs.sandbox.ebay.com/services/search/FindingService/v1"
+    )
+      .with(
+        body: failing_request,
+        headers: {
+          "X-Ebay-Soa-Global-Id" => "EBAY-US",
+          "X-Ebay-Soa-Operation-Name" => "findItemsByKeywords",
+          "X-Ebay-Soa-Request-Data-Format" => "JSON",
+          "X-Ebay-Soa-Security-Appname" => "1",
+          "X-Ebay-Soa-Service-Name" => "FindingService",
+          "X-Ebay-Soa-Service-Version" => "1.0.0"
+        }
       )
-        .with(
-          body: failing_request,
-          headers: {
-            "X-Ebay-Soa-Global-Id" => "EBAY-US",
-            "X-Ebay-Soa-Operation-Name" => "findItemsByKeywords",
-            "X-Ebay-Soa-Request-Data-Format" => "JSON",
-            "X-Ebay-Soa-Security-Appname" => "1",
-            "X-Ebay-Soa-Service-Name" => "FindingService",
-            "X-Ebay-Soa-Service-Version" => "1.0.0"
-          }
-        )
-        .to_return(status: 200, body: failing_response)
+      .to_return(status: 200, body: failing_response)
 
-      expect { subject.response("findItemsByKeywords", {}) }.to raise_error(
-        EbayRequest::Error, /Keywords value required/
-      )
-    end
-
-    it "success" do
-      expect(subject.response("findItemsByKeywords", keywords: "iPad")).to(
-      
-      )
-    end
+    expect { subject.response("findItemsByKeywords", {}) }.to raise_error(
+      EbayRequest::Error, /Keywords value required/
+    )
   end
 end
