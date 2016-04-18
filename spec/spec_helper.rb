a9aa7e0b7130e01fa56e3c5beed74e720d63f0b6
Victor Sokolov <gzigzigzeo@evilmartians.com>
2016-04-18 14:00:57 +0300

    Bundle update + simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,10 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "ebay_request"
 require "webmock/rspec"
+require "simplecov"
 
 WebMock.disable_net_connect!
+SimpleCov.start
 
 RSpec.configure do |config|
   config.order = :random
