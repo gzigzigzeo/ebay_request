9d813d15847446a4d8bbedb229f8b56a5c4327f6
Victor Sokolov <gzigzigzeo@evilmartians.com>
2016-05-18 18:49:53 +0300

    Site#to_hash

--- lib/ebay_request/site.rb
+++ lib/ebay_request/site.rb
@@ -3,10 +3,16 @@ class EbayRequest::Site
   attr_reader :code, :metric
 
   def initialize(hash)
+    @hash = hash
+
     %w(globalid id name currency language domain metric).each do |key|
       instance_variable_set(:"@#{key}", hash[key])
     end
 
     @code = globalid.gsub("EBAY-", "")
+  end
+
+  def to_hash
+    @hash
   end
 end
