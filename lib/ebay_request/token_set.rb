143035e996c8c79446698500516e868df5eaa230
Andrey Novikov <envek@envek.name>
2017-10-06 19:39:55 +0300

    Specs and documentation for TokenSet class

--- lib/ebay_request/token_set.rb
+++ lib/ebay_request/token_set.rb
@@ -1,9 +1,14 @@
+# frozen_string_literal: true
+
 require "httparty"
 
+# Holds access token from OAuth method for using with all eBay APIs
+# Retrieves new access token every time when current become outdated.
 class EbayRequest::TokenSet
   extend Dry::Initializer::Mixin
 
   class RefreshTokenExpired < EbayRequest::Error; end
+  class RefreshTokenInvalid < EbayRequest::Error; end
 
   option :access_token
   option :access_token_expires_at
@@ -11,26 +16,55 @@ class EbayRequest::TokenSet
   option :refresh_token_expires_at
   option :on_refresh, optional: true
 
+  # @!method initialize(options)
+  #   @option options [String] access_token
+  #     Short-living access token
+  #   @option options [Time]   access_token_expires_at
+  #     Timestamp of the +access_token+ expiry time.
+  #   @option options [String] refresh_token
+  #     Long-living token to get new access tokens
+  #   @option options [Time]   refresh_token_expires_at
+  #     Timestamp of the +refresh_token+ expiry time.
+  #   @option options [#call]  on_refresh
+  #     Callback. Will be called after successful renewal with two arguments:
+  #     new access token and its expiration time
+
+  # Returns access token (retrieves and returns new one if it has expired)
   def access_token
-    refresh! if access_token_expires_at < Time.now
+    refresh! if access_token_expires_at <= Time.now
     @access_token
   end
 
+  # Requests new access token, use +access_token+ to get its contents.
   def refresh!
     now = Time.now
-    raise RefreshTokenExpired if refresh_token_expires_at < now
-    response = HTTParty.post(
-        "https://api#{EbayRequest.config.sandbox? ? ".sandbox" : "" }.ebay.com/identity/v1/oauth2/token",
-        body: { grant_type: "refresh_token", refresh_token: refresh_token },
-        basic_auth: { username: EbayRequest.config.appid, password: EbayRequest.config.certid },
-        timeout: EbayRequest.config.timeout,
-    )
-    raise response.body unless response.success?
-    data = JSON.parse(response.body)
+    raise RefreshTokenExpired if refresh_token_expires_at <= now
+
+    data = refresh_token_request!
+    raise(RefreshTokenInvalid, data["error_description"]) if data.key?("error")
 
     @access_token = data["access_token"]
     @access_token_expires_at = (now + data["expires_in"])
 
     on_refresh&.call(@access_token, @access_token_expires_at)
+  end
+
+  private
+
+  def refresh_token_request!
+    user, pass = EbayRequest.config.appid, EbayRequest.config.certid
+    response = HTTParty.post(
+      token_endpoint,
+      body: { grant_type: "refresh_token", refresh_token: refresh_token },
+      basic_auth: { username: user, password: pass },
+      timeout: EbayRequest.config.timeout,
+    )
+    return JSON.parse(response.body) if [200, 400].include?(response.code)
+    raise EbayRequest::Error.new("Can't refresh token: #{response.body}")
+  end
+
+  def token_endpoint
+    environment = EbayRequest.config.sandbox? ? ".sandbox" : ""
+    "https://api#{ environment }.ebay.com/identity/v1/oauth2/token"
   end
 end
---
+++ spec/ebay_request/token_set_spec.rb
@@ -0,0 +1,97 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+describe EbayRequest::TokenSet do
+  let(:config) do
+    EbayRequest::Config.new.tap do |c|
+      c.appid = "1"
+      c.certid = "2"
+    end
+  end
+
+  let(:refresh_response) { '{"access_token":"new_token","expires_in":7200}' }
+  let!(:api) do
+    stub_request(:post, "https://api.sandbox.ebay.com/identity/v1/oauth2/token")
+      .with(
+        body: { grant_type: "refresh_token", refresh_token: "refreshing" },
+        basic_auth: %w[1 2],
+      )
+      .to_return(status: 200, body: refresh_response)
+  end
+
+  let(:access_expire)  { Time.now + 1 }
+  let(:refresh_expire) { Time.now + 1 }
+  let(:callback) { proc {} }
+
+  subject do
+    described_class.new(
+      access_token:  "old_token",  access_token_expires_at:  access_expire,
+      refresh_token: "refreshing", refresh_token_expires_at: refresh_expire,
+      on_refresh: callback,
+    )
+  end
+
+  before do
+    Timecop.freeze
+    allow(EbayRequest).to receive(:config).and_return(config)
+    allow(callback).to receive(:call).with("new_token", Time.now + 7200)
+  end
+
+  after { Timecop.return }
+
+  describe "#access_token" do
+    context "with valid access_token" do
+      it "returns access_token" do
+        expect(subject.access_token).to eq("old_token")
+        expect(api).not_to have_been_requested
+      end
+    end
+
+    context "with expired access_token" do
+      let(:access_expire)  { Time.now }
+
+      it "gets and returns a new access token" do
+        expect(subject.access_token).to eq("new_token")
+        expect(api).to have_been_requested
+      end
+    end
+  end
+
+  describe "#refresh!" do
+    context "with valid refresh token" do
+      it "requests new token from API" do
+        subject.refresh!
+        expect(api).to have_been_requested
+        expect(subject.access_token).to eq("new_token")
+        expect(subject.access_token_expires_at).to eq(Time.now + 7200)
+      end
+
+      it "calls callback" do
+        subject.refresh!
+        expect(callback).to have_received(:call)
+      end
+    end
+
+    context "with expired refresh token" do
+      let(:refresh_expire)  { Time.now }
+
+      it "raises exception" do
+        expect { subject.refresh! }.to \
+          raise_error(EbayRequest::TokenSet::RefreshTokenExpired)
+      end
+    end
+
+    context "with revoked refresh token" do
+      let(:refresh_response) do
+        '{"error":"invalid_grant","error_description":"this is fiasco"}'
+      end
+
+      it "raises exception" do
+        expect { subject.refresh! }.to raise_error(
+          EbayRequest::TokenSet::RefreshTokenInvalid, "this is fiasco"
+        )
+      end
+    end
+  end
+end
